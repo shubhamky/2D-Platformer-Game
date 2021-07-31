6b12d4ce677fb3e7d7ad26b471d1c17bbc7d3d1b
DESKTOP-F4UBJGL\ABC <shubhamyadav74242@gmail.com>
2021-07-31 17:53:25 +0530

    commit after adding lobby

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LobbyController : MonoBehaviour
+{
+    public Button playButton;
+
+    public void Awake()
+    {
+        playButton.onClick.AddListener(PlayGame);
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
